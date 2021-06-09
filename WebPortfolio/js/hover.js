5220d76cf6b7cb17649e53c20281c0b69bd628e5
JohannOlivares <60246437+JohannOlivares@users.noreply.github.com>
2021-06-09 01:14:09 -0700

    Add files via upload

---
+++ WebPortfolio/js/hover.js
@@ -0,0 +1,15 @@
+/* 
+ *  Author: Johann Olivares
+ *  File: hover.js
+ *  Date: 05/17/2020
+ *  Description: These JavaScript functions are used to
+ *               turn the project images into GIFs for
+ *               a better view of the content.
+ * */
+
+ //Function will change the url of a specifed element 
+function changeURL(element, url) {
+    element.src = url;
+    //element.style.height = "100%";
+    //element.style.width = "100%";
+}
